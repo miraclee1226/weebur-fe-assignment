95d8b36ef583dd346c55382fa7b356998dab81c0
Lin <heron6534@gmail.com>
2025-05-15 12:26:12 +0900

    move: lib/storage.ts -> utils/storage/viewMode.ts로 경로 변경 및 파일명 변경

--- src/hooks/useViewMode.ts
+++ src/hooks/useViewMode.ts
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { getViewMode, saveViewMode } from "@/lib/storage";
+import { getViewMode, saveViewMode } from "@/utils/storage/viewMode";
 import { ViewMode } from "@/types/common";
 
 /**
