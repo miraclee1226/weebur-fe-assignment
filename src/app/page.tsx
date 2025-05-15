8121de8b2ce96c60a93db2b216e6b921579b1cec
Lin <heron6534@gmail.com>
2025-05-15 14:58:48 +0900

    fix: viewMode와 스켈레톤 UI 표시 순서 불일치 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useCustomSearchParams } from "@/hooks/useCustomSearchParams";
-import { Suspense } from "react";
+import { Suspense, useEffect, useState } from "react";
 import ProductListSkeleton from "@/components/pages/main/skeleton/ProductList";
 import ErrorResetBoundary from "@/components/common/ErrorResetBoundary";
 import ErrorFallback from "@/components/common/Fallback/ErrorFallback";
@@ -10,8 +10,21 @@ import useViewMode from "@/hooks/useViewMode";
 import Gnb from "@/components/pages/main/Gnb";
 
 export default function Home() {
+  const [viewModeLoaded, setViewModeLoaded] = useState(false);
   const { searchParams, setSearchParams } = useCustomSearchParams();
   const viewMode = useViewMode();
+
+  useEffect(() => {
+    if (viewMode !== null) {
+      setViewModeLoaded(true);
+    }
+  }, [viewMode]);
+
+  if (!viewModeLoaded) {
+    return (
+      <Gnb searchParams={searchParams} setSearchParams={setSearchParams} />
+    );
+  }
 
   return (
     <>
