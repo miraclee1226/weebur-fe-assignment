5b2b559d23d3af989ed4a0c135c352e80ed710ac
Lin <heron6534@gmail.com>
2025-05-15 13:51:29 +0900

    design: Gnb 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,29 +2,26 @@
 
 import { useCustomSearchParams } from "@/hooks/useCustomSearchParams";
 import { Suspense } from "react";
-import { SearchForm } from "@/components/pages/main";
 import ProductListSkeleton from "@/components/pages/main/skeleton/ProductList";
 import ErrorResetBoundary from "@/components/common/ErrorResetBoundary";
 import ErrorFallback from "@/components/common/Fallback/ErrorFallback";
 import ProductList from "@/components/pages/main/ProductList";
 import useViewMode from "@/hooks/useViewMode";
+import Gnb from "@/components/pages/main/Gnb";
 
 export default function Home() {
   const { searchParams, setSearchParams } = useCustomSearchParams();
   const viewMode = useViewMode();
 
   return (
-    <main className="m-auto w-[1200px]">
-      <SearchForm
-        searchParams={searchParams}
-        setSearchParams={setSearchParams}
-      />
+    <>
+      <Gnb searchParams={searchParams} setSearchParams={setSearchParams} />
 
       <ErrorResetBoundary fallbackComponent={ErrorFallback}>
         <Suspense fallback={<ProductListSkeleton viewMode={viewMode} />}>
           <ProductList />
         </Suspense>
       </ErrorResetBoundary>
-    </main>
+    </>
   );
 }
